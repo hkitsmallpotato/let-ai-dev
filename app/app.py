5f7d1eb60a031c9025a01831699f44d63077f910
Lemontea <lemontea.Tom@gmail.com>
2023-05-21 07:01:47 +0800

    Chained call experiment

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ import gradio as gr
 llm = Llama(model_path="./wizard-mega-13B.ggml.q4_0.bin", n_ctx=1500) #, n_gpu_layers=40
 
 # TODO: Whatever works atm
-def run_llm(prompt):
-    output = llm(prompt, max_tokens=12, stop=["Q:", "[end of text]"], echo=True)
+def run_llm(prompt, count):
+    output = llm(prompt.format(count=count), max_tokens=12, stop=["Q:", "[end of text]"], echo=True)
     prefix_l = len(prompt)
     return output['choices'][0]['text'][prefix_l:]
 
@@ -22,9 +22,17 @@ with gr.Blocks() as software_dev_app:
         with gr.Column(scale=1):
             initial_prompt = gr.Textbox(label="Initial Prompt")
         with gr.Column(scale=1):
-            output = gr.Textbox(label="AI reply")
+            output1 = gr.Textbox(label="AI reply 1")
+            output2 = gr.Textbox(label="AI reply 2")
+            output3 = gr.Textbox(label="AI reply 3")
+            in1 = gr.Textbox(value="first", visible=False)
+            in2 = gr.Textbox(value="second", visible=False)
+            in3 = gr.Textbox(value="third", visible=False)
             ask_btn = gr.Button("Ask AI!")
-            ask_btn.click(fn=run_llm, inputs=initial_prompt, outputs=output, api_name="ask")
+            ask_btn.click(fn=run_llm, inputs=[initial_prompt, in1], outputs=output1) \
+              .success(fn=run_llm, inputs=[initial_prompt, in2], outputs=output2) \
+              .success(fn=run_llm, inputs=[initial_prompt, in3], outputs=output3)
     with gr.Tab("App scaffolding"):
         gr.Markdown("Under construction!")
+
 software_dev_app.launch()
